374a1b0b45cca4e5271ef44fdcc2ae6d3a680bdc
Sebastián Bolaños <sebasbose@gmail.com>
2024-12-05 16:23:12 -0600

    Created models for Change, CoffeeType and Payment

---
+++ coffee_machine_backend/Domain/Models/Change.cs
@@ -0,0 +1,7 @@
+﻿namespace coffee_machine_backend.Domain.Models;
+
+public class Change
+{
+    public int Amount { get; set; }
+    public Dictionary<int, int> CoinBreakdown { get; set; } = new();
+}
---
+++ coffee_machine_backend/Domain/Models/Payment.cs
@@ -0,0 +1,8 @@
+﻿namespace coffee_machine_backend.Domain.Models;
+
+public class Payment
+{
+    public int TotalAmount { get; set; }
+    public List<int> Coins { get; set; }
+    public List<int> Bills { get; set; }
+}
