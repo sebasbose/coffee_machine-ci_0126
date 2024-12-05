dc7a27ecbcff09b139acd91da0524f7ffb8dd30f
Sebastián Bolaños <sebasbose@gmail.com>
2024-12-05 16:43:24 -0600

    Created the simulated database for coffe inventory.

--- coffee_machine_backend/Application/Managers/CoffeeManager.cs
+++ coffee_machine_backend/Application/Managers/CoffeeManager.cs
@@ -5,7 +5,16 @@ using coffee_machine_backend.Domain.Models;
 
 public class CoffeeManager: ICoffeeManager
 {
-    public List<CoffeeType> GetAvailableCoffees() => [];
+    private List<CoffeeType> CoffeeInventory = new()
+    {
+        new CoffeeType {Name = "Americano", Stock = 10, Price = 950},
+        new CoffeeType {Name = "Capuchino", Stock = 8, Price = 1200},
+        new CoffeeType {Name = "Late", Stock = 10, Price = 1350},
+        new CoffeeType {Name = "Mocachino", Stock = 15, Price = 1500},
+
+    };
+
+    public List<CoffeeType> GetAvailableCoffees() => CoffeeInventory;
 
     public string PurchaseCoffee(Dictionary<string, int> order, Payment payment)
     {
