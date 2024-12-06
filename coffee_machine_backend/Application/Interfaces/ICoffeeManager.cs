cd7d607694635c858296f9036fa56ff390cc1f07
Sebastián Bolaños <sebasbose@gmail.com>
2024-12-05 22:12:55 -0600

    improvements to controller, manager and database.

--- coffee_machine_backend/Application/Interfaces/ICoffeeManager.cs
+++ coffee_machine_backend/Application/Interfaces/ICoffeeManager.cs
@@ -1,9 +1,10 @@
 ﻿namespace coffee_machine_backend.Application.Interfaces;
 
+using coffee_machine_backend.Domain.Database;
 using coffee_machine_backend.Domain.Models;
 
 public interface ICoffeeManager
 {
-    List<CoffeeType> GetAvailableCoffees();
-    string PurchaseCoffee(Dictionary<string, int> order, Payment payment);
+    List<CoffeeType> GetAvailableCoffees(Database database);
+    string PurchaseCoffee(Database database, Dictionary<string, int> order, Payment payment);
 }
