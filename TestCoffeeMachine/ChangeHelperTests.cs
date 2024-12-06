711917b23b28352c61fa300713c771c95d870fba
Sebastián Bolaños <sebasbose@gmail.com>
2024-12-05 22:47:41 -0600

    Added ChangeHelper UnitTests and a demonstration video of the frontend and backend running in realtime.

---
+++ TestCoffeeMachine/ChangeHelperTests.cs
@@ -0,0 +1,54 @@
+﻿namespace TestCoffeeMachine;
+
+using coffee_machine_backend.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class ChangeHelperTests
+{
+    [Test]
+    public void CalculateChange_ExactChange_ShouldReturnCorrectBreakdown()
+    {
+        // Arrange
+        var availableCoins = new Dictionary<int, int>
+            {
+                { 500, 10 },
+                { 100, 10 },
+                { 50, 10 },
+                { 25, 10 }
+            };
+
+        // Act
+        var change = ChangeHelper.CalculateChange(675, availableCoins);
+
+        // Assert
+        Assert.IsNotNull(change);
+        Assert.AreEqual(675, change.Amount);
+        Assert.AreEqual(1, change.CoinBreakdown[500]);
+        Assert.AreEqual(1, change.CoinBreakdown[100]);
+        Assert.AreEqual(1, change.CoinBreakdown[50]);
+        Assert.AreEqual(1, change.CoinBreakdown[25]);
+    }
+
+    [Test]
+    public void CalculateChange_NotEnoughCoins_ShouldReturnNull()
+    {
+        // Arrange
+        var availableCoins = new Dictionary<int, int>
+            {
+                { 500, 0 },
+                { 100, 1 },
+                { 50, 1 },
+                { 25, 1 }
+            };
+
+        // Act
+        var change = ChangeHelper.CalculateChange(675, availableCoins);
+
+        // Assert
+        Assert.IsNull(change);
+    }
+}
--- TestCoffeeMachine/CoffeeManagerTests.cs
+++ TestCoffeeMachine/CoffeeManagerTests.cs
@@ -1,4 +1,6 @@
-﻿using coffee_machine_backend.Application.Managers;
+﻿namespace TestCoffeeMachine;
+
+using coffee_machine_backend.Application.Managers;
 using coffee_machine_backend.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -6,73 +8,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestCoffeeMachine
+internal class CoffeeManagerTests
 {
-    internal class CoffeeManagerTests
+    private CoffeeManager _coffeeManager;
+
+    [SetUp]
+    public void Setup()
     {
-        private CoffeeManager _coffeeManager;
+        _coffeeManager = new CoffeeManager();
+    }
 
-        [SetUp]
-        public void Setup()
+    [Test]
+    public void GetAvailableCoffees_ShouldReturnInitialStock()
+    {
+        // Act
+        var coffees = _coffeeManager.GetAvailableCoffees();
+
+        // Assert
+        Assert.AreEqual(4, coffees.Count);
+        Assert.AreEqual("Americano", coffees[0].Name);
+        Assert.AreEqual(10, coffees[0].Stock);
+    }
+
+    [Test]
+    public void PurchaseCoffee_ValidOrder_ShouldReduceStockAndReturnChange()
+    {
+        // Arrange
+        var order = new Dictionary<string, int>
         {
-            _coffeeManager = new CoffeeManager();
-        }
-
-        [Test]
-        public void GetAvailableCoffees_ShouldReturnInitialStock()
+            { "Americano", 2 }
+        };
+        var payment = new Payment
         {
-            // Act
-            var coffees = _coffeeManager.GetAvailableCoffees();
+            TotalAmount = 2000,
+            Coins = new List<int> { 500, 500 },
+            Bills = new List<int> { 1000 }
+        };
 
-            // Assert
-            Assert.AreEqual(4, coffees.Count);
-            Assert.AreEqual("Americano", coffees[0].Name);
-            Assert.AreEqual(10, coffees[0].Stock);
-        }
+        // Act
+        var result = _coffeeManager.PurchaseCoffee(order, payment);
 
-        [Test]
-        public void PurchaseCoffee_ValidOrder_ShouldReduceStockAndReturnChange()
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual("Su vuelto es de: 100 colones. Desglose: 1 moneda de 100", result);
+    }
+
+    [Test]
+    public void PurchaseCoffee_InvalidOrder_ShouldThrowArgumentException()
+    {
+        // Arrange
+        var order = new Dictionary<string, int>
         {
-            // Arrange
-            var order = new Dictionary<string, int>
-            {
-                { "Americano", 2 }
-            };
-            var payment = new Payment
-            {
-                TotalAmount = 2000,
-                Coins = new List<int> { 500, 500 },
-                Bills = new List<int> { 1000 }
-            };
-
-            // Act
-            var result = _coffeeManager.PurchaseCoffee(order, payment);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.AreEqual("Su vuelto es de: 100 colones. Desglose: 1 moneda de 100", result);
-        }
-
-        [Test]
-        public void PurchaseCoffee_InvalidOrder_ShouldThrowArgumentException()
+            { "Americano", 11 }
+        };
+        var payment = new Payment
         {
-            // Arrange
-            var order = new Dictionary<string, int>
-            {
-                { "Americano", 11 }
-            };
-            var payment = new Payment
-            {
-                TotalAmount = 12500,
-                Coins = new List<int> { 500, 500, 500, 500, 500 },
-                Bills = new List<int> { 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 }
-            };
+            TotalAmount = 12500,
+            Coins = new List<int> { 500, 500, 500, 500, 500 },
+            Bills = new List<int> { 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 }
+        };
 
-            // Act
-            var result = _coffeeManager.PurchaseCoffee(order, payment);
+        // Act
+        var result = _coffeeManager.PurchaseCoffee(order, payment);
 
-            // Assert
-            Assert.AreEqual("No hay suficientes Americano en stock.", result);
-        }
+        // Assert
+        Assert.AreEqual("No hay suficientes Americano en stock.", result);
     }
 }
